d9607e1249bc8c02caaa9bae7a87293cde9a9b50
nilspin <gooney28@gmail.com>
2018-08-15 13:54:59 +0530

    Append to previous commit

--- Application.cpp
+++ Application.cpp
@@ -8,12 +8,13 @@
 SDL_Event event;
 
 Application::Application() {
-  rectangle.resize(640*480);
+  rectangle.resize(bufferWidth*bufferHeight);
   left = stbi_load("assets/left.jpg", &imgWidth, &imgHeight, &channels, 2);
   right = stbi_load("assets/right.jpg", &imgWidth, &imgHeight, &channels, 2);
   if(left == nullptr) {cout<<"could not read first image file!"<<endl; exit(0);}
   if(right == nullptr) {cout<<"could not read second image file!"<<endl; exit(0);}
-  cam.setPosition(glm::vec3(0, 0, 0));
+  //cam.setPosition(glm::vec3(320, 240, 300));
+  cam.setPosition(glm::vec3(0,0,100));
   glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
   setupShaders();
   setupBuffers();
@@ -104,15 +105,19 @@ void Application::run() {
 
 
     //first depthMap
-    glDrawArrays(GL_POINTS, 0, 640*480);
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexVBO);
+    //glDrawElements(GL_TRIANGLE_STRIP, indices.size(), GL_UNSIGNED_INT, 0);
+    glBindVertexArray(drawVAO);
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, bufferWidth*bufferHeight);
+    //glDrawArrays(GL_POINTS, 0, bufferWidth*bufferHeight);
     //second depthMap
     //glUniform1i(drawShader->uniform("depthTexture"), 1);
     //glUniformMatrix4fv(drawShader->uniform("MVP"), 1, false, glm::value_ptr(proj*view*localTransform));
     //glUniform3f(drawShader->uniform("shadeColor"), 0, 1, 0);
-    //glDrawArrays(GL_POINTS, 0, 640*480);
+    //glDrawArrays(GL_POINTS, 0, bufferWidth*bufferHeight);
     //
     mat4 newMVP = proj*view;//*scaleMat
-    
+
     window.swap();
   }
 }
@@ -155,22 +160,52 @@ void Application::setupTextures() {
 
 void Application::setupBuffers() {
 
-  for(int i=0;i<640;++i){
-    for(int j=0;j<480;++j){
-      rectangle[i*480+j] = vec2(i,j);
+  for(int i=0;i<bufferWidth;++i){
+    for(int j=0;j<bufferHeight;++j){
+      rectangle[i*480+j] = vec3(i,j, 0);
     }
   }
+  //Now build indices
+  indices.push_back(0);
+  indices.push_back(bufferWidth);
+  for(int i=1;i<=bufferHeight;++i){
+    for(int j=1;j<bufferWidth;++j){
+      indices.push_back((i-1)*bufferWidth + j);
+      indices.push_back(i*bufferWidth + j);
+
+      if(j==bufferWidth-1) {
+        indices.push_back(i*bufferWidth +j);
+        indices.push_back(i*bufferWidth);
+        indices.push_back(i*bufferWidth);
+        indices.push_back((i+1)*bufferWidth);
+      }
+    }
+  }
+  indices.pop_back();
+  uint indicesCount = indices.size();
+
+  std::cout<<"Indices Count : "<<indicesCount<<"\n";
+  //==========================
   //Create Vertex Array Object
   glGenVertexArrays(1, &drawVAO);
   glBindVertexArray(drawVAO);
 
   glGenBuffers(1, &drawVBO);
   glBindBuffer(GL_ARRAY_BUFFER, drawVBO);
-  glBufferData(GL_ARRAY_BUFFER, imgWidth * imgHeight * sizeof(glm::vec2), rectangle.data(), GL_STATIC_DRAW);
+  glBufferData(GL_ARRAY_BUFFER, bufferWidth * bufferHeight * sizeof(glm::vec3), rectangle.data(), GL_STATIC_DRAW);
   // //Assign attribs
   glEnableVertexAttribArray(drawShader->attribute("position"));
-  glVertexAttribPointer(drawShader->attribute("position"), 2, GL_FLOAT, GL_FALSE, 0, 0);
+  glVertexAttribPointer(drawShader->attribute("position"), 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+  //Bind index buffer
+  glGenBuffers(1, &indexVBO);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexVBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size()*sizeof(uint), indices.data(), GL_STATIC_DRAW);
+
   glBindVertexArray(0);	//unbind VAO
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
 }
 
 
